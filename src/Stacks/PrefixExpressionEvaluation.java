d16b1d5cd3a25d25bbb1097642ed03ec7b48db7d
mohitbhasin <mohit.bhasin16@gmail.com>
2024-08-30 15:34:00 +0530

    PrefixEvaluation using reverse Postfix
    iterate right to left using postfix evaluation

--- src/Stacks/PrefixExpressionEvaluation.java
+++ src/Stacks/PrefixExpressionEvaluation.java
@@ -2,64 +2,55 @@ import java.util.*;
 
 class PrefixExpressionEvaluation {
 	public static void main(String args[]) {
-		// String[] expression = {"/","-","15","*","/","3","2","5","5"}; //1.5;
-		// String[] expression = {"+","*","2","3","-","4","5"}; //5
-		String[] expression = {"-","*","5","6","/","7","2"}; //26.5
-		System.out.println(evaluate(expression));
+		List<String[]> list = new ArrayList<>();
+		list.add(new String[] {"/","-","15","*","/","3","2","5","5"}); //1.5;
+		list.add(new String[] {"+","*","2","3","-","4","5"}); //5
+		list.add(new String[] {"-","*","5","6","/","7","2"}); //26.5
+		list.add(new String[] {"+","-","*","3","4","5","/","6","2"}); //10
+		list.add(new String[] {"-","*","+","2","3","4","/","5","2",}); //17.5
+		
+		for(String[] expression: list) {
+			System.out.println(evaluate(expression));
+		}
     }
 
     public static double evaluate(String[] elements) {
-    	Stack<String> stack = new Stack<>();
-    	Set<String> set = new HashSet<>();
-    	set.add("/");
-    	set.add("*");
-    	set.add("+");
-    	set.add("-");
-
-    	for(String element: elements) {
-    		// curr is operator
-    		if(set.contains(element)) {
-    			stack.push(element);
-    		} 
-    		// curr is operand
-    		else {
-    			// stack not empty and stack top is operator
-    			if(stack.isEmpty() || set.contains(stack.peek())) {
-    				stack.push(element);
-    			} else {
-    				while(!set.contains(stack.peek())) {
-    					String a = stack.pop();
-	    				String b = element;
-	    				String operator = stack.pop();
-	    				double result = 0;
-	    				switch (operator) {
-	    					case "/" : {
-	    						result=Double.parseDouble(a) / Double.parseDouble(b);
-	    						break;
-	    					}
-	    					case "*" : {
-	    						result=Double.parseDouble(a) * Double.parseDouble(b);
-	    						break;
-	    					}
-	    					case "+" : {
-	    						result=Double.parseDouble(a) + (double) Double.parseDouble(b);
-	    						break;
-	    					}
-	    					case "-" : {
-	    						result=Double.parseDouble(a) - (double) Double.parseDouble(b);
-	    						break;
-	    					}
-	    				}
-	    				if(stack.isEmpty() || set.contains(stack.peek())) {
-	    					stack.push(""+result);
-	    					break;
-	    				} else {
-	    					element = ""+result;
-	    				}
-    				}
+    	if(elements.length)
+    	Stack<Double> stack = new Stack<>();
+    	double a;
+    	double b;
+    	for(int i=elements.length-1; i>=0; i--) {
+    		String element = elements[i];
+    		switch (element) {
+    			case "/": {
+    				a=stack.pop();
+    				b=stack.pop();
+    				stack.push(a/b);
+    				break;
     			}
-    		}
+	    		case "*": {
+    				a=stack.pop();
+    				b=stack.pop();
+    				stack.push(a*b);
+    				break;
+    			}
+	    		case "+": {
+	    				a=stack.pop();
+	    				b=stack.pop();
+	    				stack.push(a+b);
+	    				break;
+    			}
+    			case "-": {
+	    				a=stack.pop();
+	    				b=stack.pop();
+	    				stack.push(a-b);
+	    				break;
+    			}
+	    		default: {
+	    			stack.push(Double.valueOf(element));
+	    		}
+	    	}
     	}
-    	return Double.parseDouble(stack.peek());
+    	return stack.peek();
     }
 }
